fe1ab821db231cd14327bc7470dad4d9394530a9
inaki <inaki@ideaknow.com>
2012-11-08 19:54:44 +0100

    tv project

---
+++ BlackJackTVRemote/gen/com/example/google/tv/remote/blackjack/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.google.tv.remote.blackjack;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
